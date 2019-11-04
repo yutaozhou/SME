8980533b7d4def28eb40455a4eb8ea216f6254b3
Ansgar Wehrhahn <ansgar.wehrhahn@physics.uu.se>
2019-11-04 11:12:50 +0100

    Fix test

--- test/test_sme_structure.py
+++ test/test_sme_structure.py
@@ -25,9 +25,9 @@ def test_empty_structure():
     assert isinstance(empty.version, str)
     assert empty.teff is None
     assert empty.logg is None
-    assert empty.vmic is None
-    assert empty.vmac is None
-    assert empty.vsini is None
+    assert empty.vmic is 0
+    assert empty.vmac is 0
+    assert empty.vsini is 0
 
     assert empty.nseg is None
     assert empty.wave is None
