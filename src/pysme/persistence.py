b0da69a93a47f5cf4aa294004b1705bcad7b8359
Ansgar Wehrhahn <ansgar.wehrhahn@physics.uu.se>
2020-02-11 13:56:58 +0100

    Fix IDL SME structure requirements

--- src/pysme/persistence.py
+++ src/pysme/persistence.py
@@ -198,9 +198,13 @@ def write_as_idl(sme):
     """
 
     wind = np.cumsum(sme.wave.shape[1]) + 1
+    vrad_flag = {"none": -2, "whole": -1, "each": 0}[sme.vrad_flag]
+    cscale_flag = {"none": -3, "fix": -2, "constant": 0, "linear": 1, "quadratic": 1,}[
+        sme.cscale_flag
+    ]
 
     idl_fields = {
-        "version": sme.version,
+        "version": 5.1,
         "id": sme.id,
         "teff": sme.teff,
         "grav": sme.logg,
@@ -208,16 +212,10 @@ def write_as_idl(sme):
         "vmic": sme.vmic,
         "vmac": sme.vmac,
         "vsini": sme.vsini,
-        "vrad": sme.vrad.tolist(),
-        "vrad_flag": {"none": -2, "whole": -1, "each": 0}[sme.vrad_flag],
-        "cscale": sme.cscale.tolist(),
-        "cscale_flag": {
-            "none": -3,
-            "fix": -2,
-            "constant": 0,
-            "linear": 1,
-            "quadratic": 1,
-        }[sme.cscale_flag],
+        "vrad": sme.vrad.tolist() if vrad_flag == 0 else sme.vrad[0],
+        "vrad_flag": vrad_flag,
+        "cscale": sme.cscale.tolist() if cscale_flag in [0, 1] else sme.cscale[0],
+        "cscale_flag": cscale_flag,
         "gam6": sme.gam6,
         "h2broad": int(sme.h2broad),
         "accwi": sme.accwi,
@@ -235,14 +233,14 @@ def write_as_idl(sme):
         "lineref": save_as_binary(sme.linelist.reference),
         "short_line_format": {"short": 1, "long": 2}[sme.linelist.lineformat],
         "wran": sme.wran.tolist(),
-        "mu": sme.mu.tolist(),
+        "mu": sme.mu.tolist() if sme.nmu > 1 else sme.mu[0],
         "wave": save_as_binary(sme.wave.ravel()),
         "wind": wind.tolist(),
         "sob": save_as_binary(sme.spec.ravel()),
         "uob": save_as_binary(sme.uncs.ravel()),
         "mob": save_as_binary(sme.mask.ravel()),
         "obs_name": "",
-        "obs_type": "",
+        "obs_type": 0,
         "iptype": sme.iptype,
         "ipres": sme.ipres,
         # "ip_x": sme.ip_x,
