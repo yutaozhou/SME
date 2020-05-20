b459d77acefa9752a23c664310ea66bb3292b591
Ansgar Wehrhahn <ansgar.wehrhahn@physics.uu.se>
2020-05-20 19:50:59 +0200

    travis

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -32,5 +32,5 @@ done
 # Install packages and test
 for PYBIN in /opt/python/cp3*/bin/; do
     "${PYBIN}/pip" install pysme-astro --no-index -f /io/wheelhouse
-    (cd "$HOME"; "${PYBIN}/pytest")
+    "${PYBIN}/pytest"
 done
