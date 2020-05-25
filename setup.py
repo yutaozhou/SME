02a67c390f4415f411cac645993934c33df2e370
Ansgar Wehrhahn <ansgar.wehrhahn@physics.uu.se>
2020-05-25 18:39:28 +0200

    Convert f strings

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 
 import sys
 
-if sys.version_info.major == 3 and sys.version_info.minor < 6:
+if True:  # sys.version_info.major == 3 and sys.version_info.minor < 6:
     from convert_fstrings import convert
 
     print(
